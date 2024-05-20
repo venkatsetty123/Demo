4a4da27ad00e5148db33477068886307523aa60a
venkatsetty123 <168698027+venkatsetty123@users.noreply.github.com>
2024-05-20 10:43:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// let decreaseBtn=document.getElementById("decreaseBtn");
+// let ResetBtn=document.getElementById("ResetBtn");
+// let IncreaseBtn=document.getElementById("IncreaseBtn");
+// let countLabel=document.getElementById("countLabel");
+// let count=0;
+// IncreaseBtn.onclick=function(){
+//     count++;
+//     countLabel.textContent=count;
+// }
+// decreaseBtn.onclick=function(){
+//     count--;
+//     countLabel.textContent=count;
+// }
+// ResetBtn.onclick=function(){
+//     count=0;
+//     countLabel.textContent=count;
+// }
